fe48103bd2c5217e710b988f72cd49e9f79c74d7
Morgan Zhong <z5361117@ad.unsw.edu.au>
2025-06-18 18:07:41 +1000

    Testing modifying main and app files

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,10 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-    
+    <App/>
+      <header>
+        hello
+      </header>
+
   </StrictMode>,
 )
